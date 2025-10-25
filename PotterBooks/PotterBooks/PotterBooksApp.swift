f1287a6f147c7f4c3c3cab1a758b0b898e84e222
Rohit <rohitchugh15@gmail.com>
2025-10-25 22:25:14 +0530

    added xcode project

---
+++ PotterBooks/PotterBooks/PotterBooksApp.swift
@@ -0,0 +1,21 @@
+//
+//  PotterBooksApp.swift
+//  PotterBooks
+//
+//  Created by Rohit Chugh on 25/10/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct PotterBooksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
